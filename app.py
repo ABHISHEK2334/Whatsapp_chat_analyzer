a1dbad9ec8fc82cd139ac2e27db3f3948985b4d2
ABHISHEK2334 <132339463+ABHISHEK2334@users.noreply.github.com>
2024-10-13 17:54:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,31 +44,12 @@ page_bg_img = f"""
 </style>
 """
 
-# Google Analytics and Google Tag Manager Script
-google_tag_manager = """
-<!-- Google Tag Manager -->
-<script>
-(function(w,d,s,l,i){
-    w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-    var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-    f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JLDQDFN');
-</script>
-<!-- End Google Tag Manager -->
 
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JLDQDFN"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
-"""
 
 # Embed background style and GTM script
 st.markdown(f"""
     {page_bg_img}
-    {google_tag_manager}
+    
 """, unsafe_allow_html=True)
 
 st.markdown("""
