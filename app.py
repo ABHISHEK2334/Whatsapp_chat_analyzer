e4f962927cd78d49cc53aab0cdc46e7f29290ad7
ABHISHEK2334 <132339463+ABHISHEK2334@users.noreply.github.com>
2024-10-14 11:04:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -49,14 +49,15 @@ page_bg_img = f"""
 """
 gtm_script = """
 <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JLDQDFN');</script>
-<!-- End Google Tag Manager -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JLDQDFN"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9B5RTGQGM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V9B5RTGQGM');
+</script>
 </head>
 """
 
